5cb434b27549cb26a73878103ed5a0febbfcc6d6
TopexGuy <topexguy@gmail.com>
2026-03-31 07:50:44 +0000

    sky-miuicamera: adapt for sky

--- extract-files.py
+++ extract-files.py
@@ -18,7 +18,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/xiaomi/miuicamera-marble',
+    'device/xiaomi/sky-miuicamera',
 ]
 
 
@@ -44,7 +44,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'miuicamera-marble',
+    'sky-miuicamera',
     'xiaomi',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
